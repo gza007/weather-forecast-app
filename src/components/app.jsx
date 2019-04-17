3adb66efa5a493ec40625ad5b74bb75d03b41055
Geevan Patel <geevan.peter.patel@gmail.com>
2019-04-18 00:43:42 +0100

    first commit, location details step completed

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import LocationDetails from './location-details';
 
-const App = props => <h1>{`Hello ${props.name}!`}</h1>;
+const App = props => (
+  <LocationDetails
+    city={props.location.city}
+    country={props.location.country}
+  />
+);
 
 App.propTypes = {
-  name: PropTypes.string.isRequired,
+  location: PropTypes.shape({
+    city: PropTypes.string,
+    country: PropTypes.string,
+  }).isRequired,
 };
 
 export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -2,5 +2,6 @@ import 'raf/polyfill';
 import React from 'react';
 import { render } from 'react-dom';
 import App from './components/app';
+import { location } from './data/forecast.json';
 
-render(<App name="JHEEZ" />, document.getElementById('root'));
+render(<App location={location} />, document.getElementById('root'));
