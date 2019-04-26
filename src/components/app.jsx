a415400e35e64d7d5b611ad4c0e01619f286b255
Geevan Patel <geevan.peter.patel@gmail.com>
2019-04-26 23:10:39 +0100

    Changed App from function component to a class, and then added some state!

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -5,16 +5,29 @@ import ForecastSummaries from './forecast-summaries';
 import ForecastDetails from './forecast-details';
 import '../styles/app.scss';
 
-const App = props => (
-  <div className="forecast">
-    <LocationDetails
-      city={props.location.city}
-      country={props.location.country}
-    />
-    <ForecastSummaries forecasts={props.forecasts} />
-    <ForecastDetails forecast={props.forecasts[0]} />
-  </div>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedDate: this.props.forecasts[0].date,
+    };
+  }
+
+  render() {
+    const selectedForecast =
+      this.props.forecasts.find(forecast => forecast.date === this.state.selectedDate);
+    return (
+      <React.Fragment>
+        <LocationDetails
+          city={this.props.location.city}
+          country={this.props.location.country}
+        />
+        <ForecastSummaries forecasts={this.props.forecasts} />
+        <ForecastDetails forecast={selectedForecast} />
+      </React.Fragment>
+    );
+  }
+}
 
 App.propTypes = {
   location: PropTypes.shape({
